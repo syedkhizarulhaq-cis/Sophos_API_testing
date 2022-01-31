beefcb9286b96077c16e8424b11db6ffcb42aee7
nullb0i <33244888+syedkhizarulhaq-cis@users.noreply.github.com>
2022-01-31 21:22:38 +0500

    Update api.py

--- api.py
+++ api.py
@@ -1,9 +1,31 @@
 #!/bin/python3
 import requests
+import xml.etree.ElementTree as ET
 
+
+#Update the IP list in intel IP list 
+#response=requests.get("""https://172.16.16.16:4444/webconsole/APIController?reqxml=<Request><Login><Username>adminapi</Username>
+#<Password>Test@3706653</Password></Login><Set operation="update"><IPHost><Name>Threat-intel-IPs</Name>
+#<IPFamily>IPv4</IPFamily><HostType>IPList</HostType>
+#<ListOfIPAddresses>178.128.116.50</ListOfIPAddresses>
+#</IPHost></Set></Request>""", verify=False)
+#print(response.text)
+
+
+
+#Get the IP list for Threat intel IPs:
+response=requests.get("""https://172.16.16.16:4444/webconsole/APIController?reqxml=<Request><Login><Username>adminapi</Username><Password>Test@3706653</Password></Login><Get><IPHost><Name>Threat-intel-IPs</Name><IPFamily>IPv4</IPFamily><HostType>IPList</HostType><ListOfIPAddresses></ListOfIPAddresses></IPHost></Get></Request>""",verify=False)
+myroot = ET.fromstring(response.text)
+for x in myroot.iter('ListOfIPAddresses'):
+	print(x.text)
+ipaddr=x.text
+#ipaddrr="5.5.5.5"
+print(response.text)
+
+ipaddrrr = ipaddr + "," + ipaddrr
+#Update the IP list in Threat intel IPs:
 response=requests.get("""https://172.16.16.16:4444/webconsole/APIController?reqxml=<Request><Login><Username>adminapi</Username>
 <Password>Test@3706653</Password></Login><Set operation="update"><IPHost><Name>Threat-intel-IPs</Name>
 <IPFamily>IPv4</IPFamily><HostType>IPList</HostType>
-<ListOfIPAddresses>1.2.3.4,2.3.5.4,178.128.116.50</ListOfIPAddresses>
-</IPHost></Set></Request>""", verify=False)
+<ListOfIPAddresses>"""+ipaddrrr+"""</ListOfIPAddresses></IPHost></Set></Request>""",verify=False)
 print(response.text)
