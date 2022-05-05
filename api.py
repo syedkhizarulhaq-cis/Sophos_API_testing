e6d3a192d92344e8d02e64904427841f346116f3
nullb0i <33244888+syedkhizarulhaq-cis@users.noreply.github.com>
2022-05-05 19:09:25 +0500

    Update api.py

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ import xml.etree.ElementTree as ET
 
 #Update the IP list in intel IP list 
 #response=requests.get("""https://172.16.16.16:4444/webconsole/APIController?reqxml=<Request><Login><Username>adminapi</Username>
-#<Password>Test@3706653</Password></Login><Set operation="update"><IPHost><Name>Threat-intel-IPs</Name>
+#<Password>Password</Password></Login><Set operation="update"><IPHost><Name>Threat-intel-IPs</Name>
 #<IPFamily>IPv4</IPFamily><HostType>IPList</HostType>
 #<ListOfIPAddresses>178.128.116.50</ListOfIPAddresses>
 #</IPHost></Set></Request>""", verify=False)
@@ -14,7 +14,7 @@ import xml.etree.ElementTree as ET
 
 
 #Get the IP list for Threat intel IPs:
-response=requests.get("""https://172.16.16.16:4444/webconsole/APIController?reqxml=<Request><Login><Username>adminapi</Username><Password>Test@3706653</Password></Login><Get><IPHost><Name>Threat-intel-IPs</Name><IPFamily>IPv4</IPFamily><HostType>IPList</HostType><ListOfIPAddresses></ListOfIPAddresses></IPHost></Get></Request>""",verify=False)
+response=requests.get("""https://172.16.16.16:4444/webconsole/APIController?reqxml=<Request><Login><Username>adminapi</Username><Password>password</Password></Login><Get><IPHost><Name>Threat-intel-IPs</Name><IPFamily>IPv4</IPFamily><HostType>IPList</HostType><ListOfIPAddresses></ListOfIPAddresses></IPHost></Get></Request>""",verify=False)
 myroot = ET.fromstring(response.text)
 for x in myroot.iter('ListOfIPAddresses'):
 	print(x.text)
@@ -25,7 +25,7 @@ print(response.text)
 ipaddrrr = ipaddr + "," + ipaddrr
 #Update the IP list in Threat intel IPs:
 response=requests.get("""https://172.16.16.16:4444/webconsole/APIController?reqxml=<Request><Login><Username>adminapi</Username>
-<Password>Test@3706653</Password></Login><Set operation="update"><IPHost><Name>Threat-intel-IPs</Name>
+<Password>password</Password></Login><Set operation="update"><IPHost><Name>Threat-intel-IPs</Name>
 <IPFamily>IPv4</IPFamily><HostType>IPList</HostType>
 <ListOfIPAddresses>"""+ipaddrrr+"""</ListOfIPAddresses></IPHost></Set></Request>""",verify=False)
 print(response.text)
